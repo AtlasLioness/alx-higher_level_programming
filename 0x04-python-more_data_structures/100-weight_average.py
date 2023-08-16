d569e00fa088776ac22c92001eb6621ed23b4956
AtlasLioness <narjisaliane@gmail.com>
2023-08-16 20:58:47 +0000

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+
+    result = 0
+    weight = 0
+    if my_list:
+        for s, w in my_list:
+            result += s * w
+            weight += w
+        result = result / weight
+    return result
