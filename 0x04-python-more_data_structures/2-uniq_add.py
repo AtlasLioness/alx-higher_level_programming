30d0c07d3df0e262f872951465279a9833870219
AtlasLioness <narjisaliane@gmail.com>
2023-08-16 18:23:39 +0000

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
 
-    return sum(set(my_list))
-
+    new_list = list(set(my_list))
+    result = 0
+    for i in range(len(new_list)):
+        result += new_list[i]
+    return result
