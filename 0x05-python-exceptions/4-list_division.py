91408e60fc4524eb36e8c0141d99a2ce29db25b0
AtlasLioness <narjisaliane@gmail.com>
2023-08-28 19:15:39 +0000

    4. Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+
+    i = 0
+    new_list = []
+
+    for i in range(0, list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+
+        except TypeError:
+            print("wrong type")
+            new_list[i] = 0
+            continue
+
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+            continue
+
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+            continue
+
+        finally:
+            i = i + 1
+            continue
+    return new_list
