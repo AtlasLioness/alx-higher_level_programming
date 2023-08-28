a5c76d0a5e0dab9a7ec6bb33cf613c844e4e08c8
AtlasLioness <narjisaliane@gmail.com>
2023-08-28 19:18:27 +0000

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except TypeError:
             print("wrong type")
-            new_list[i] = 0
+            new_list.append(0)
             continue
 
         except ZeroDivisionError:
