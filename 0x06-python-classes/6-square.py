77b43f2214cfc7f17393c8f0bd702db27d8b9792
AtlasLioness <narjisaliane@gmail.com>
2023-08-29 22:25:10 +0000

    6. Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """defining square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """initialization function"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns area of square"""
+        return self.__size ** 2
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, type((0, 0))):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        space = ""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                space += "\n"
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    space += " "
+                for j in range(self.__size):
+                    space += "#"
+                space += "\n"
+            print("{}".format(space), end="")
