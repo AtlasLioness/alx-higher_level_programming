424b22b0c89fa76210f703e02955a8a0e0daf9ed
AtlasLioness <narjisaliane@gmail.com>
2023-09-08 18:50:18 +0000

    3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines function that prints a square with #"""
+
+
+def print_square(size):
+    """prints square in #"""
+    if not isinstance(size, int) :
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print("")
