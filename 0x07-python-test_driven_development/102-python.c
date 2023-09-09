ad720bcaa765a8f2d9822ad06785658a5f6db6fc
AtlasLioness <narjisaliane@gmail.com>
2023-09-09 01:57:14 +0000

    8. CPython #3: Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -12,16 +12,16 @@ void print_python_string(PyObject *p)
 	printf("[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		printf(" [ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 	len = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf(" type: compact ascii\n");
+		printf("  type: compact ascii\n");
 	else
-		printf(" type: compact unicode object\n");
-	printf(" length: %ld\n", len);
-	printf(" value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", len);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
 }
 
