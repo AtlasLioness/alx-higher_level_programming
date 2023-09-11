60d0205bc193f607dc317c016b5973bb7c157496
AtlasLioness <narjisaliane@gmail.com>
2023-09-11 20:38:09 +0000

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with one attribute"""
+    def area(self):
+        """raises exception with message"""
+        raise Exception("area() is not implemented")
