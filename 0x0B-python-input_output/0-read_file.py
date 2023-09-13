663683a34832fe21f3e4b49b6d7c1f68105cd5fe
AtlasLioness <narjisaliane@gmail.com>
2023-09-13 14:40:24 +0000

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""Read file module"""
 
 def read_file(filename=""):
+    """reads then prints text file"""
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read(), end="")
