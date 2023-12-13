f6fb1642338238c2f3c83bcb2c18f134e6446ca1
AtlasLioness <narjisaliane@gmail.com>
2023-12-13 18:26:11 +0000

    task 14

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
---Write a script that removes all records with a score <= 5 in the table second_table
+-- Write a script that removes records where score is less thqn 5
 DELETE FROM second_table WHERE score <= 5;
