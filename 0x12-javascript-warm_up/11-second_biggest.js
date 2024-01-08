c16cde34f3613186cabbfc363335a2626839c906
AtlasLioness <narjisaliane@gmail.com>
2024-01-08 16:41:38 +0000

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // searches the second biggest interger in a list of args
 
 if (process.argv.length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   const list = process.argv.sort();
   console.log(list.reverse()[1]);
