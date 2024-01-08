f9245569c742078f4e5b8af5e7a86ffce572206b
AtlasLioness <narjisaliane@gmail.com>
2024-01-08 13:14:28 +0000

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints 3 lines
+
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
+
+while (index < 3) {
+  console.log(myString[index]);
+  index++;
+}
