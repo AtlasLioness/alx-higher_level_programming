f36c8311f994e4fcd0283072c6f3697ac077b23b
AtlasLioness <narjisaliane@gmail.com>
2024-01-08 16:01:28 +0000

    10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that computes and prints a factorial
+
+function factorial (a) {
+  if (isNaN(a) || (a === 1)) {
+    return (1);
+  }
+  return (a * (factorial(a - 1)));
+}
+console.log(factorial(parseInt(process.argv[2])));
