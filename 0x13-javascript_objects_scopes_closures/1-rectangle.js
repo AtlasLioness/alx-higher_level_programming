8d0ef5f23b92c922c9b211c000e3528389cab0bf
AtlasLioness <narjisaliane@gmail.com>
2024-01-09 22:28:12 +0000

    1. Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// class Rectangle that defines rectangle with 2 instance attributes width and height
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
