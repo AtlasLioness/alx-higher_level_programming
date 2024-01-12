b6b1c7b5a85eeef7be80a3ef886b662332d92aa1
AtlasLioness <narjisaliane@gmail.com>
2024-01-12 11:44:25 +0000

    task 0 to 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-''' Script that lists all stats from datatbase hbtn_0e_0_usa '''
+'''
+Script that lists all stats with a name starting with N from db hbtn_0e_0_usa
+'''
 
 import MySQLdb
 import sys
@@ -11,12 +13,12 @@ db = MySQLdb.connect(
         passwd=sys.argv[2],
         db=sys.argv[3]
 )
-
 cur = db.cursor()
-numrows = cur.execute("SELECT * FROM states")
+numrows = cur.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cur.fetchall()
 for row in rows:
     index, state = row
-    print("({}, '{}')".format(index, state))
+    if state[0] == 'N':
+        print("({}, '{}')".format(index, state))
 cur.close()
 db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Script that takes in an argument and displays all values
+in th states table of hbtn_0e_0_usa where name matches the argument.
+'''
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+)
+
+cur = db.cursor()
+name = sys.argv[4]
+
+# format to create the SQL query with the user input
+q = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(name)
+
+numrows = cur.execute(q)
+rows = cur.fetchall()
+
+for row in rows:
+    index, state = row
+    print("({}, {})".format(index, state))
+
+cur.close()
+db.close()
