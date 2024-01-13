4ad03dc38e045789b889f33c0762b982a50bdc03
AtlasLioness <narjisaliane@gmail.com>
2024-01-13 17:41:43 +0000

    task 1 corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,19 +6,20 @@ Script that lists all stats with a name starting with N from db hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(
+if __name__ == "__main__":
+    db = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
-)
-cur = db.cursor()
-numrows = cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    index, state = row
-    if state[0] == 'N':
-        print("({}, '{}')".format(index, state))
-cur.close()
-db.close()
+        )
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        index, state = row
+        if state[0] == 'N':
+            print("({}, '{}')".format(index, state))
+    cur.close()
+    db.close()
