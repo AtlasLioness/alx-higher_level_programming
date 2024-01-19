f1c5d2020f102196c60cdc9ec45e567f9b8543bf
AtlasLioness <narjisaliane@gmail.com>
2024-01-19 14:32:57 +0000

    fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
         )
     cur = db.cursor()
-    name = sys.argv[4]
 
     # format to create the SQL query with the user input
-    q = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(name)
-    numrows = cur.execute(q)
+    q = """
+    SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC;"""
+    q = q.format(argv[4])
+    cur.execute(q)
     rows = cur.fetchall()
     for row in rows:
-        index, state = row
-        print("({}, '{}')").format(index, state)
+        print(row)
     cur.close()
     db.close()
