039a6f1039962f3614cb81b5d02126d65cb0b0ba
AtlasLioness <narjisaliane@gmail.com>
2024-04-22 16:01:29 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // searches the second biggest int in the list of args
 
-if (process.argv.length < 3) {
-  console.log('0');
-} else if (process.argv.length === 3) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
   const sorting = process.argv.sort();
   console.log(sorting.reverse()[1]);
