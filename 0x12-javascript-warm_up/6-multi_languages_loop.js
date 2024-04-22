2a8430cabd70bb7cb75361d8d734168c312abff1
AtlasLioness <narjisaliane@gmail.com>
2024-04-22 13:49:25 +0000

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints 3 lines using an array of string an a loop
+
+const strings = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < 3; i++) {
+  console.log(strings[i]);
+}
