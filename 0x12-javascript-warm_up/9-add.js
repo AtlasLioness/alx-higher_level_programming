dea0b1a9bb39883b97565ec65832638d059d4f5b
AtlasLioness <narjisaliane@gmail.com>
2024-04-22 14:30:42 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,11 @@
 // prints the adition of 2 integer using funtion add(a, b)
 
 // function definition
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
-console.log(add(arg1, arg2)); 
+console.log(add(arg1, arg2));
