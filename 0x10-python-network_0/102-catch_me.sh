a509ab75cc8f896adcf1af9fc7221921cac82c2a
AtlasLioness <narjisaliane@gmail.com>
2024-05-13 00:07:28 +0000

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/ makes request to 0.0.0.0:5000/catch_me to cause response You got me!
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "You got me!"
+curl -sL "0.0.0.0:5000/catch_me" -X PUT -H "You got me!"
